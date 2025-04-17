03e442d9ec9297eeb3c8a1534bc09fe6bede01af
ArpitaDeodikar01 <168329514+ArpitaDeodikar01@users.noreply.github.com>
2025-04-17 21:38:25 +0530

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -2,12 +2,20 @@ public class Edge {
     Location from;
     Location to;
     double safetyRating;
-    double distance;
+    double distance; // New parameter to represent the distance between locations
 
-    Edge(Location from, Location to, double safetyRating,double distance) {
+    // Constructor now includes distance
+    public Edge(Location from, Location to, double safetyRating, double distance) {
         this.from = from;
         this.to = to;
         this.safetyRating = safetyRating;
         this.distance = distance;
     }
+
+    // Getter for distance
+    public double getDistance() {
+        return distance;
+    }
+
+    // You can also add other getter methods if necessary for other attributes
 }
